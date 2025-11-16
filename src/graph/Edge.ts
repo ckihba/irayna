d29d2d8add99f91ecf9f4034751de90046c2c8fd
ckihba <ckihba@gmail.com>
2025-11-16 09:52:36 +0530

    Create Edge.ts

---
+++ src/graph/Edge.ts
@@ -0,0 +1,9 @@
+// Edge class placeholder for irayna visual editor.
+export class Edge {
+  from: string;
+  to: string;
+  constructor(from: string, to: string) {
+    this.from = from;
+    this.to = to;
+  }
+}
